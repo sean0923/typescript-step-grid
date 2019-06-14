aed87f8e3aa56b86714ea3c38f1a01f1a889bfca
Sean <iamseanhong@gmail.com>
2019-06-13 22:02:31 -0700

    First glance why developer like typescript

--- fetchData/index.ts
+++ fetchData/index.ts
@@ -2,6 +2,22 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(({ data }) => {
-  console.log('data: ', data);
+  const todo = data as Todo;
+
+  const id = todo.Id;
+  const title = todo.titles;
+  const finished = todo.finished;
+
+  console.log(`
+    Id: ${id}
+    Title: ${title}
+    Done: ${finished}
+  `);
 });

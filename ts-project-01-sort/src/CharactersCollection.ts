4260666322809108995636d85f356a1dd67a3153
Sean <iamseanhong@gmail.com>
2019-06-16 21:40:46 -0700

    Implement CharactersCollection

---
+++ ts-project-01-sort/src/CharactersCollection.ts
@@ -0,0 +1,22 @@
+export class CharactersCollection {
+  constructor(public str: string) {}
+
+  get length(): number {
+    return this.str.length;
+  }
+
+  isLeftBigger(leftIdx: number, rightIdx: number): boolean {
+    return this.str.toLowerCase()[leftIdx] > this.str.toLowerCase()[rightIdx];
+  }
+
+  swap(leftIdx: number, rightIdx: number): void {
+    const leftChar = this.str[leftIdx];
+    const rightChar = this.str[rightIdx];
+
+    // A + r + l + B
+    const front = this.str.slice(0, leftIdx);
+    const back = this.str.slice(rightIdx + 1);
+
+    this.str = front + rightChar + leftChar + back;
+  }
+}
--- ts-project-01-sort/src/index.ts
+++ ts-project-01-sort/src/index.ts
@@ -1,9 +1,19 @@
 import { NumberCollection } from './NumberCollection';
+import { CharactersCollection } from './CharactersCollection';
 import { Sorter } from './Sorter';
 
 const numCollect = new NumberCollection([2, 1, 7, 10, 4]);
 const sorter = new Sorter(numCollect);
 
+const str = 'FaEDzSbc';
+
+const charCollet = new CharactersCollection(str);
+
 sorter.sort();
 
 console.log('numCollect: ', numCollect.data);
+
+const charSorter = new Sorter(charCollet);
+
+charSorter.sort();
+console.log('charCollet: ', charCollet);

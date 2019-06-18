bdfb54186916fccc4eaac0b52699491886acd15a
Sean <iamseanhong@gmail.com>
2019-06-18 00:18:08 -0700

    Count win

--- ts-project-02-stats/src/index.ts
+++ ts-project-02-stats/src/index.ts
@@ -1,1 +1,18 @@
-console.log('a');
+import fs from 'fs';
+
+const rawMatches = fs.readFileSync('./football.csv', { encoding: 'utf-8' });
+const parsedMatches = rawMatches.split('\n').map((str: string): string[] => str.split(','));
+
+let manWin = 0;
+
+for (let parsedMatch of parsedMatches) {
+  if (parsedMatch[1] === 'Man United' && parsedMatch[5] === 'H') {
+    manWin++;
+  }
+
+  if (parsedMatch[2] === 'Man United' && parsedMatch[5] === 'A') {
+    manWin++;
+  }
+}
+
+console.log(`Man United win ${manWin}`);

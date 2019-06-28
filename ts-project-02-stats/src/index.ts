e5371b91d0632a2e15134699def6b6ac31620e6d
Sean Hong <iamseanhong@gmail.com>
2019-06-28 00:18:03 -0700

    Remove map error

--- ts-project-02-stats/src/index.ts
+++ ts-project-02-stats/src/index.ts
@@ -1,13 +1,14 @@
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { WinAnalysis } from './WinAnalysis';
+import { Summary } from './Summary';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
 
 const csvFileReader = new CsvFileReader('./football.csv');
 const matchReader = new MatchReader(csvFileReader);
 
-const winAnalysis = new WinAnalysis('Man United');
-
 matchReader.load();
 
-const res = winAnalysis.run(matchReader.matches);
-console.log('res: ', res);
+const summary = new Summary(new WinAnalysis('Man United'), new ConsoleReport());
+
+summary.buildAndPrintReport(matchReader.matches);

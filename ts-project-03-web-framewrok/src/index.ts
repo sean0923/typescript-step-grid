ce0c1c5c75446ec47e7b6038fb730c3455622a8e
Sean Hong <iamseanhong@gmail.com>
2019-06-30 17:05:15 -0700

    Setup json-server n concurrently

--- ts-project-03-web-framewrok/src/index.ts
+++ ts-project-03-web-framewrok/src/index.ts
@@ -2,9 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Sean', age: 29 });
 
-user.set({ name: 'Yo' });
+user.on('click', () => {
+	console.log('yoyo');
+	console.log('yoyo');
+	console.log('yoyo');
+});
 
-const name = user.get('name');
-console.log('name: ', name);
-const age = user.get('age');
-console.log('age: ', age);
+user.trigger('click');
--- ts-project-03-web-framewrok/src/models/User.ts
+++ ts-project-03-web-framewrok/src/models/User.ts
@@ -18,5 +18,20 @@ export class User {
 		Object.assign(this.data, propKeyValPair);
 	}
 
-	on(eventName: string, callback: Callback): void {}
+	on(eventName: string, callback: Callback): void {
+		const tempCallbacks = this.events[eventName] || [];
+		tempCallbacks.push(callback);
+		this.events[eventName] = tempCallbacks;
+	}
+
+	trigger(eventName: string): void {
+		const callbacks = this.events[eventName];
+		if (!callbacks || callbacks.length === 0) {
+			return;
+		}
+
+		callbacks.forEach((callback) => {
+			callback();
+		});
+	}
 }

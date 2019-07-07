edd47274e51753a9827aa0cea7e6be1d0ef78ad8
Sean <iamseanhong@gmail.com>
2019-07-06 21:18:43 -0600

    Implement eventing sync and attributes class

--- ts-project-03-web-framewrok/src/models/Sync.ts
+++ ts-project-03-web-framewrok/src/models/Sync.ts
@@ -1,30 +1,23 @@
 import axios, { AxiosPromise } from 'axios';
 
-import { UserProps } from './User';
+interface HasId {
+  id?: number;
+}
 
-// const HOST = 'http://localhost:3000';
+export class Sync<T extends HasId> {
+  constructor(public rootURL: string) {}
 
-export class Sync {
-	constructor(public rootURL: string) {}
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootURL}/${id}`);
+  }
 
-	fetch(id: number): AxiosPromise {
-		return axios.get(`${this.rootURL}/${id}`);
-		// .then(({ data }) => {
-		// 	this.data = data;
-		// 	console.log('data: ', data);
-		// })
-		// .catch((err) => {
-		// 	console.error('err: ', err);
-		// });
-	}
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    if (id) {
+      // if data is at backend -> update
+      return axios.put(`${this.rootURL}/${id}`);
+    }
 
-	save(data: UserProps): AxiosPromise {
-		const { id } = data;
-		if (id) {
-			// if data is at backend -> update
-			return axios.put(`${this.rootURL}/${id}`);
-		}
-
-		return axios.post(this.rootURL, data);
-	}
+    return axios.post(this.rootURL, data);
+  }
 }
--- ts-project-03-web-framewrok/src/models/User.ts
+++ ts-project-03-web-framewrok/src/models/User.ts
@@ -1,3 +1,9 @@
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
+
+const HOST = 'http://localhost:3000';
+
 export interface UserProps {
   id?: number;
   name?: string;
@@ -5,7 +11,11 @@ export interface UserProps {
 }
 
 export class User {
-  // events: { [key: string]: Callback[] } = {};
+  public eventing = new Eventing();
+  public sync = new Sync(HOST);
+  public attributes;
 
-
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }

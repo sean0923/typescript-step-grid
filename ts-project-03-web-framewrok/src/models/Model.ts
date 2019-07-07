02b6befc46d9336b0956378268f1ccaf381a3e31
Sean <iamseanhong@gmail.com>
2019-07-07 17:25:09 -0600

    Add static method build user to User

--- ts-project-03-web-framewrok/src/models/Model.ts
+++ ts-project-03-web-framewrok/src/models/Model.ts
@@ -1,6 +1,9 @@
-import { AxiosPromise } from 'axios';
+import { AxiosPromise, AxiosResponse } from 'axios';
 
 type Callback = () => void;
+type HasId = {
+  id?: number;
+};
 
 interface ModelAttributes<T> {
   get<K extends keyof T>(key: K): T[K];
@@ -19,4 +22,48 @@ interface ModelSync<T> {
   save(data: T): AxiosPromise;
 }
 
-export class Model {}
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private eventing: ModelEventing,
+    private sync: ModelSync<T>
+  ) {}
+
+  get on() {
+    return this.eventing.on;
+  }
+
+  get trigger() {
+    return this.eventing.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(userProp: T): void {
+    this.attributes.set(userProp);
+    this.eventing.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without id');
+    }
+
+    this.sync.fetch(id).then((resp: AxiosResponse) => {
+      this.set(resp.data);
+    });
+  }
+
+  save(): void {
+    const userProp = this.attributes.getAll();
+
+    this.sync.save(userProp).then((resp: AxiosResponse) => {
+      console.log('?');
+      this.trigger('save');
+    });
+  }
+}
--- ts-project-03-web-framewrok/src/models/User.ts
+++ ts-project-03-web-framewrok/src/models/User.ts
@@ -1,7 +1,7 @@
+import { Model } from './Model';
 import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { ApiSync } from './ApiSync';
 import { Attributes } from './Attributes';
-import { AxiosResponse } from 'axios';
 
 const HOST = 'http://localhost:3000/users';
 
@@ -11,50 +11,8 @@ export interface UserProps {
   age?: number;
 }
 
-export class User {
-  public eventing: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(HOST);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  get on() {
-    return this.eventing.on;
-  }
-
-  get trigger() {
-    return this.eventing.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(userProp: UserProps): void {
-    this.attributes.set(userProp);
-    this.eventing.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without id');
-    }
-
-    this.sync.fetch(id).then((resp: AxiosResponse) => {
-      this.set(resp.data);
-    });
-  }
-
-  save(): void {
-    const userProp = this.attributes.getAll();
-
-    this.sync.save(userProp).then((resp: AxiosResponse) => {
-			console.log('?');
-      this.trigger('save');
-    });
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(new Attributes<UserProps>(attrs), new Eventing(), new ApiSync<UserProps>(HOST));
   }
 }

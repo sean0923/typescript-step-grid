72ebfd5ef2a06beee5a613686b1f6e48ddbf2f99
Sean <iamseanhong@gmail.com>
2019-07-11 21:58:11 -0700

    Bind events

--- ts-project-03-web-framewrok/src/views/UserForm.ts
+++ ts-project-03-web-framewrok/src/views/UserForm.ts
@@ -4,6 +4,7 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onMouseEnter,
     };
   }
 
@@ -11,18 +12,36 @@ export class UserForm {
     console.log('button clicked');
   }
 
+  onMouseEnter(): void {
+    console.log('MouseEnter');
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Submit</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    Object.keys(eventsMap).forEach((eventKey) => {
+      const [event, selector] = eventKey.split(':');
+      const callback = eventsMap[eventKey];
+
+      fragment.querySelectorAll(selector).forEach((node) => {
+        node.addEventListener(event, callback);
+      });
+    });
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }

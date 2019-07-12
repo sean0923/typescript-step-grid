0e2ce45eb9539180fde9b57081f3f1bb9ee9c738
Sean <iamseanhong@gmail.com>
2019-07-11 22:08:57 -0700

    Clean up code

--- ts-project-03-web-framewrok/src/index.ts
+++ ts-project-03-web-framewrok/src/index.ts
@@ -2,6 +2,6 @@ import { User } from './models/User';
 import { UserForm } from './views/UserForm';
 
 const rootElement = document.getElementById('root');
-const userForm = new UserForm(rootElement, User.buildUser({'name': 'Sean', age: 30});
+const userForm = new UserForm(rootElement, User.buildUser({ name: 'Sean', age: 30 }));
 
 userForm.render();

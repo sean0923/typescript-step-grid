089b98b695dfc6eaf1dc84229c5b13f23dc1bb1c
Sean Hong <iamseanhong@gmail.com>
2019-07-13 19:40:24 -0700

    Set up post routes n parse req.body with body parser

--- ts-project-04-ts-express/src/index.ts
+++ ts-project-04-ts-express/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 const PORT = 3001;
--- ts-project-04-ts-express/src/routes/loginRoutes.ts
+++ ts-project-04-ts-express/src/routes/loginRoutes.ts
@@ -2,12 +2,25 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/login', (req: Request, res: Response) => {
 	res.send(`
-    <div>
-      <h1>Yeah ~~</h1>
-    </div>
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+	const { email, password } = req.body;
+	res.send(`${email}, ${password}`);
 });
 
 export { router };
